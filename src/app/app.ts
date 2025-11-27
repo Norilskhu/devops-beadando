d8b96b45db272a5d7fb8539045db1ff7fb2c3e1d
Norilskhu <norilskvagyok@gmail.com>
2025-11-27 21:13:08 +0100

    Cím, youtube és github linkek módosítása

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('devops-beadando');
+  protected readonly title = signal('DEVOPS beadandó feladat');
 }
